bb5218484d3c9b1b7f2b3efa3bea4ce4d87afa9b
luis Colina <79709581+Silkiercomet@users.noreply.github.com>
2021-03-20 10:20:43 -0400

    delete feature added

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 let list = document.querySelector(".lista");
 let btn = document.querySelector(".btn")
-const deleteElement = () => {
-    list.removeChild(item);
-}
+
 const addElement = () => {
     let input = document.querySelector("#in").value;
     let item = document.createElement("li");
     let node = document.createTextNode(input);
     let item2 = document.createElement("button");
+
+    const deleteElement = () => {
+        list.removeChild(item);
+        list.removeChild(item2);
+    }
+    if(input.length > 0){
     item.appendChild(node);
     list.appendChild(item);
-    list.appendChild(item2);
+    item.appendChild(item2);
     document.querySelector("#in").value = ""
+    item2.textContent = "delete"
+    item2.style.marginLeft = ".5rem"
+    }
     item2.addEventListener("click", deleteElement)
 }
 
